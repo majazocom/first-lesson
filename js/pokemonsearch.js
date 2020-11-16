24110be1d3b52dbc07958a434a56931f759e6818
Maja Gedda <maja.gedda@zocom.se>
2020-11-16 13:31:01 +0100

    search

--- js/pokemonsearch.js
+++ js/pokemonsearch.js
@@ -1,12 +1,35 @@
 //Add eventlistener to the button
+let pokemons;
+
+//här hämtar vi all data från vår json-fil
+let fetchStuff = () => {
+    fetch('../assets/pokemon.json')
+    .then(response => response.json()) //här gör vi om vår respons till objekt
+    .then(data => {
+        //här stoppar vi in resultatet i vår pokemon-variabel
+        pokemons = data.pokemons
+
+    })
+    .catch((error) => {
+        console.log('Error: ', error);
+    });
+}
+
+fetchStuff(); //kallar vi på vår fetch-metod.
 
 document.getElementById("search-button").addEventListener('click', () => {
     //Get the value from the input - save into variable
-    console.log(fetch('../assets/pokemon.json'));
+    let input = document.getElementById('search-input').value;
 
-    fetch('../assets/pokemon.json')
-        .then(response => response.json())
-        .catch((error) => {
-            console.log('Error: ', error);
-        });
+    for (let i = 0; i < pokemons.length; i++) {
+        
+        if (pokemons[i].name === input.toLowerCase()) {
+            console.log('true');
+        }
+    }
+
+    // if (pokemons.some(pokemon => pokemon.name === input.toLowerCase())) {
+    //     console.log('true');
+    // }
+
 });
