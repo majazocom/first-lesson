a8d86a789aef0bf50adf591823aaf07fd5681f7c
Maja Gedda <maja.gedda@zocom.se>
2020-11-16 11:08:41 +0100

    a fetch

--- js/pokemonsearch.js
+++ js/pokemonsearch.js
@@ -1,6 +1,10 @@
 //Add eventlistener to the button
 document.getElementById("search-button").addEventListener('click', () => {
     //Get the value from the input - save into variable
-    console.log('hejhej');
+    console.log(fetch('../assets/pokemon.json'));
+
+    fetch('../assets/pokemon.json')
+        .then(data => data.json())
+        .then(console.log);
 })
 
